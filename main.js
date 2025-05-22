87093d39895589213c373562c9551a1a97bf9673
exon <throwawayact69xd@gmail.com>
2025-05-21 23:20:46 -0400

    sync file

--- main.js
+++ main.js
@@ -10,40 +10,23 @@ function getFile(fileName) {
     }
 }
 
-async function fetchApi(word) {
-    const url = "https://twoblade.com/api/username/check?username=" + word;
-    try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
-        }
-        const json = await response.json();
-        return json;
-    } catch (error) {
-        console.error(`Error fetching "${word}": ${error.message}`);
-    }
-}
-
-let unclaimed = "CHECKED @ " + new Date().toString()+"\n";
-
 async function checkFile(fileName) {
     const content = getFile(fileName);
     if (!content) return;
 
     const words = content.split("\n");
     for (const word of words) {
-        const data = await fetchApi(word.trim());
-        console.log(word.trim(), data);
-        if (!data.error) unclaimed += word.trim() + "\n";
-    }
+        const trimmedWord = word.trim();
+        if (!trimmedWord) continue;
 
-    fs.writeFile('avaliable.txt', unclaimed, err => {
-        if (err) {
-          console.error(err);
-        } else {
-            console.log("We did it :)")
+        const data = await fetchApi(trimmedWord);
+        console.log(trimmedWord, data);
+        if (data && data.available === true){
+            unclaimed += trimmedWord + "\n";
+            fs.writeFileSync('avaliable.txt', unclaimed);
         }
-    });      
+    }
+    console.log("Finished checking all words.");
 }
 
 checkFile("words.txt");
